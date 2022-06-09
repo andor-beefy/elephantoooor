b03c21d78a3c69089be665d5ad9616cf25a82f07
Tomas Sedlon <tomas.sedlon@adastragrp.com>
2022-06-09 18:51:04 +0200

    init commit

---
+++ helper-hardhat-config.js
@@ -0,0 +1,18 @@
+const networkConfig = {
+  31337: {
+    name: "localhost",
+  },
+  // Price Feed Address, values can be obtained at https://docs.chain.link/docs/reference-contracts
+  // Default one is ETH/USD contract on Kovan
+  4: {
+    name: "rinkeby",
+    ethUsdPriceFeed: "0x9326BFA02ADD2366b30bacB125260Af641031331",
+  },
+}
+
+const developmentChains = ["hardhat", "localhost"]
+
+module.exports = {
+  networkConfig,
+  developmentChains,
+}
